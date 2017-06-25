2d14054149b800b497da98a6a86d56ec72ea36e3
Jesse Barreto <jesseh.barreto@gmail.com>
2017-06-25 18:03:59 -0300

    * Project 7 - main Done
    * Project 7 - BackGround subtraction Start

--- Project7/main.cpp
+++ Project7/main.cpp
@@ -33,16 +33,50 @@
 #include <opencv2/opencv_modules.hpp>
 #include <opencv2/legacy/legacy.hpp>
 #include <iostream>
+#include <chrono>
 
 #include "project7.h"
+                                                            //  a   d   e   t       or  a   d   e   t
+#define DEFAULT_FILE1 "Data/highwayI_raw.avi"               //  1.5 20 25   35      or  1   15  15  49
+#define DEFAULT_FILE2 "Data/Bsb-EPGU.mov"                   //  55  23  25  80      or  1   12  12  34
+#define DEFAULT_FILE3 "Data/Bsb-EixoRodoviarioSul.mov"      //  10  12  12  35      or  66  44  44  98
+#define DEFAULT_FILE4 "Data/viptraffic.mp4"                 //  1   10  15  28      or  15  5   5   36
 
-#define DEFAULT_VIDEO_FILE "Data/highwayI_raw.avi"
-#define VIDEO_SPEED_RATE 0.5
-#define OPTICAL_FLOW_ALPHA 1.0
+#define DEFAULT_VIDEO_FILE DEFAULT_FILE4
+#define VIDEO_SPEED_RATE 1 // Can not be zero
 
+#define OPTICAL_FLOW_ALGORITHM_HS 1
+
+#define ADAPTATIVE_BILATERAL_FILTER 0
+#define BILATERAL_FILTER 0
+#define BLUR_FILTER 1
+#define BLUR_BOX_SIZE 15
+
+#define USE_OTSU 0
+#define USE_BHS 1
+
+#define USE_MORPH_OPEN_CLOSE 1
 
 #define ORIGINAL_WIN_NAME "Original Video"
 #define FLOW_WIN_NAME "Optical Flow Video"
+#define FILTERED_FLOW_WIN_NAME "Filtered Optical Flow Video"
+#define NORMALIZED_FLOW_WIN_NAME "Normalized Optical Flow Values"
+#define BINARY_NORMAL_WIN_NAME "Binary Normal Flow Video"
+#define BINARY_MORPH_WIN_NAME "Binary Morphed Flow Video"
+#define RESULT_WIN_NAME "Result Video"
+
+#define DEFAULT_ALPHA 8000
+#define DEFAULT_DILATE_BOX_SIZE 15
+#define DEFAULT_ERODE_BOX_SIZE 30
+
+static int alphaTrack = 1000;
+static double globalHSAlpha = alphaTrack / 1000;
+static int globalDilateSize = DEFAULT_DILATE_BOX_SIZE;
+static int globalErodeSize = DEFAULT_ERODE_BOX_SIZE;
+static bool globalTurnOnDilate = false;
+static bool globalTurnOnErode = false;
+
+static void trackBarHandler(int , void *);
 
 int main(int argc, char **argv)
 {
@@ -63,54 +97,177 @@ int main(int argc, char **argv)
 
     // Run Video
     cv::namedWindow(ORIGINAL_WIN_NAME, CV_WINDOW_NORMAL);
-    cv::namedWindow(FLOW_WIN_NAME, CV_WINDOW_NORMAL);
     cv::resizeWindow(ORIGINAL_WIN_NAME, 640, 480);
+    cv::namedWindow(FLOW_WIN_NAME, CV_WINDOW_NORMAL);
     cv::resizeWindow(FLOW_WIN_NAME, 640, 480);
-
-    // Flag to show indicating it has started
-    bool isInitializing = true;
+    cv::namedWindow(FILTERED_FLOW_WIN_NAME, CV_WINDOW_NORMAL);
+    cv::resizeWindow(FILTERED_FLOW_WIN_NAME, 640, 480);
+    cv::namedWindow(NORMALIZED_FLOW_WIN_NAME, CV_WINDOW_NORMAL);
+    cv::resizeWindow(NORMALIZED_FLOW_WIN_NAME, 640, 480);
+    cv::namedWindow(BINARY_NORMAL_WIN_NAME, CV_WINDOW_NORMAL);
+    cv::resizeWindow(BINARY_NORMAL_WIN_NAME, 640, 480);
+    cv::namedWindow(BINARY_MORPH_WIN_NAME, CV_WINDOW_NORMAL);
+    cv::resizeWindow(BINARY_MORPH_WIN_NAME, 640, 480);
+    cv::namedWindow(RESULT_WIN_NAME, CV_WINDOW_NORMAL);
+    cv::resizeWindow(RESULT_WIN_NAME, 640, 480);
+    cv::createTrackbar("HS Alpha", RESULT_WIN_NAME, &alphaTrack, 200000, trackBarHandler);
+    cv::createTrackbar("Dilate Size", RESULT_WIN_NAME, &globalDilateSize, 100, trackBarHandler);
+    cv::createTrackbar("Erode Size", RESULT_WIN_NAME, &globalErodeSize, 100, trackBarHandler);
 
     // Collection of Frames
     cv::Mat frame; // Current Frame
-    cv::Mat grayFrame; // Current Frame in Grayscale
+    cv::Mat currentFrame; // Current Frame in Grayscale
     cv::Mat previousFrame; // Previous Frame in Grayscale
     cv::Mat totalFlow; // Flow values at X,Y Directions and its variances
     cv::Mat flowX, flowY; // Flow values at X and Y separated
-    cv::Mat flowFrame;
+    cv::Mat blurredFlowX, blurredFlowY; // blurred Flow values at X and Y separated
+    cv::Mat flowFrame, blurFlowFrame; // Flow Grid Frames
+    cv::Mat normalFlow; // Normalized Flow Values
+    cv::Mat flowMag, flowAng;
+    cv::Mat binaryNormalFlow, binaryMorphFlow; // Image Binarized
+    std::vector<std::vector<cv::Point>> contours;
+    cv::Mat resultImage;
+
+    // First Frame
+    video >> previousFrame;
+    cv::cvtColor(previousFrame, previousFrame, CV_BGR2GRAY);
 
     for (;;) {
         // Read the frames from video
-        if (isInitializing) {
-            isInitializing = false;
-            video >> previousFrame;
-            cv::cvtColor(previousFrame, previousFrame, CV_BGR2GRAY, CV_8UC1);
-            video >> frame;
-            cv::cvtColor(frame, grayFrame, CV_BGR2GRAY, CV_8UC1);
-        } else {
-            previousFrame = grayFrame;
-            video >> frame;
-            cv::cvtColor(frame, grayFrame, CV_BGR2GRAY, CV_8UC1);
-        }
+        video >> frame;
+        cv::cvtColor(frame, currentFrame, CV_BGR2GRAY);
 
         // Loop the video
-        if (frame.empty()) {
+        if (currentFrame.empty()) {
             video.set(CV_CAP_PROP_POS_FRAMES, 0);
-            isInitializing = true;
             continue;
         }
 
-        // Exit if user press something
-        if (cv::waitKey(VIDEO_SPEED_RATE * 30) >= 0) {
+        // Exit if user press space
+        if (cv::waitKey((1/VIDEO_SPEED_RATE) * 30) >= 0) {
             break;
         }
 
-        getMotionMap(flowFrame, previousFrame, grayFrame, OPTICAL_FLOW_ALPHA, &flowX, &flowY, &totalFlow);
+        // Start Timer
+        auto start = std::chrono::high_resolution_clock::now();
 
+        // Obtain Motion Map
+#if OPTICAL_FLOW_ALGORITHM_HS
+        motionMapHSImage(flowFrame, frame,  previousFrame, currentFrame, globalHSAlpha, &flowX, &flowY, &totalFlow);
+#else
+        // Obtain Motion Map
+        motionMapFImage(flowFrame, frame, previousFrame, currentFrame, &totalFlow);
+        std::vector<cv::Mat> flows;
+        cv::split(totalFlow, flows);
+        flowX = flows[0].clone();
+        flowY = flows[1].clone();
+#endif
+
+        // Filter the Motion Map
+#if ADAPTATIVE_BILATERAL_FILTER
+        cv::Mat flowX8b, flowY8b;
+        flowX.convertTo(flowX8b, CV_8U);
+        flowY.convertTo(flowY8b, CV_8U);
+        cv::adaptiveBilateralFilter(flowX8b, blurredFlowX, cv::Size(BLUR_BOX_SIZE, BLUR_BOX_SIZE), 50);
+        cv::adaptiveBilateralFilter(flowY8b, blurredFlowY, cv::Size(BLUR_BOX_SIZE, BLUR_BOX_SIZE), 50);
+        blurredFlowX.convertTo(blurredFlowX, CV_32F);
+        blurredFlowY.convertTo(blurredFlowY, CV_32F);
+#elif BLUR_FILTER
+        cv::blur(flowX, blurredFlowX, cv::Size(BLUR_BOX_SIZE, BLUR_BOX_SIZE));
+        cv::blur(flowY, blurredFlowY, cv::Size(BLUR_BOX_SIZE, BLUR_BOX_SIZE));
+#elif BILATERAL_FILTER
+        cv::bilateralFilter(flowX, blurredFlowX, 4, 50, 50, cv::BORDER_DEFAULT);
+        cv::bilateralFilter(flowY, blurredFlowY, 4, 50, 50, cv::BORDER_DEFAULT);
+#endif
+        drawOpticalFlowImage(blurFlowFrame, blurredFlowX, blurredFlowY, frame, 5, CV_RGB(255, 0, 0));
+
+        // Normalize Magnitude
+        cv::cartToPolar(blurredFlowX, blurredFlowY, flowMag, flowAng, true);
+        cv::normalize(flowMag, normalFlow, 0, 255, cv::NORM_MINMAX, CV_8U);
+
+        // Binaryze Magnitude based on Flow Map Histogram
+#if USE_OTSU
+        cv::threshold(normalFlow, binaryNormalFlow, 0, 255, cv::THRESH_BINARY + cv::THRESH_OTSU);
+#elif USE_BHS
+        balancedHistogramThreshold(normalFlow, binaryNormalFlow);
+#else
+        cv::threshold(normalFlow, binaryNormalFlow, 200, 255, cv::THRESH_BINARY);
+#endif
+
+        // Morphologic Filter - Dilate and Erode
+#if USE_MORPH_OPEN_CLOSE
+        if (globalTurnOnErode && globalTurnOnDilate) {
+            cv::dilate(binaryNormalFlow, binaryMorphFlow,
+                       cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(globalDilateSize,
+                                                                             globalDilateSize)));
+            cv::erode(binaryMorphFlow, binaryMorphFlow,
+                       cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(globalErodeSize,
+                                                                             globalErodeSize)));
+        } else if (!globalTurnOnDilate && globalTurnOnErode) {
+            cv::erode(binaryNormalFlow, binaryMorphFlow,
+                       cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(globalErodeSize,
+                                                                             globalErodeSize)));
+        } else if (globalTurnOnDilate && !globalTurnOnErode) {
+            cv::dilate(binaryNormalFlow, binaryMorphFlow,
+                       cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(globalDilateSize,
+                                                                             globalDilateSize)));
+        } else {
+            binaryMorphFlow = binaryNormalFlow.clone();
+        }
+#else
+        if (!globalTurnOffDilate) {
+            cv::dilate(binaryNormalFlow, binaryMorphFlow,
+                       cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(globalDilateSize,
+                                                                             globalDilateSize)));
+            cv::morphologyEx(binaryMorphFlow, binaryMorphFlow, cv::MORPH_CLOSE,
+                             cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(globalMorphBoxSize,
+                                                                                   globalMorphBoxSize)));
+        } else {
+            cv::morphologyEx(binaryNormalFlow, binaryMorphFlow, cv::MORPH_CLOSE,
+                             cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(globalMorphBoxSize,
+                                                                                   globalMorphBoxSize)));
+        }
+        cv::morphologyEx(binaryMorphFlow, binaryMorphFlow, cv::MORPH_OPEN,
+                         cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(globalMorphBoxSize,
+                                                                               globalMorphBoxSize)));
+#endif
+        // Find Cars Contours
+        cv::findContours(binaryMorphFlow, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+
+        // Draw Contours
+        cv::cvtColor(currentFrame, resultImage, CV_GRAY2BGR);
+        cv::drawContours(resultImage, contours, -1, cv::Scalar(0, 255, 255));
+        for (int i = 0; i < contours.size(); i++) {
+            cv::rectangle(resultImage, boundingRect(cv::Mat(contours[i])), cv::Scalar(255, 0, 0));
+        }
+
+
+        // Show Images
         cv::imshow(ORIGINAL_WIN_NAME, frame);
         cv::imshow(FLOW_WIN_NAME, flowFrame);
+        cv::imshow(FILTERED_FLOW_WIN_NAME, blurFlowFrame);
+        cv::imshow(NORMALIZED_FLOW_WIN_NAME, normalFlow);
+        cv::imshow(BINARY_NORMAL_WIN_NAME, binaryNormalFlow);
+        cv::imshow(BINARY_MORPH_WIN_NAME, binaryMorphFlow);
+        cv::imshow(RESULT_WIN_NAME, resultImage);
+
+        // Update Image
+        previousFrame = currentFrame.clone();
+
+        // Print Time
+        auto end = std::chrono::high_resolution_clock::now();
+        auto dt = 1.e-6 * std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
+        std::clog << dt << " ms elapsed" << std::endl;
     }
 
-	return 0;
+    return 0;
 }
 
+void trackBarHandler(int , void *)
+{
+    alphaTrack = std::max(alphaTrack, 1);
+    globalHSAlpha = alphaTrack / 1000;
 
+    globalTurnOnDilate = !(globalDilateSize < 2);
+    globalTurnOnErode = !(globalErodeSize < 2);
+}
--- Project7/project7.h
+++ Project7/project7.h
@@ -5,7 +5,52 @@
 #include <opencv2/opencv_modules.hpp>
 #include <opencv2/legacy/legacy.hpp>
 
+#include <limits>
+
+// Functions Definitions
+void drawOpticalFlowImage (cv::Mat& flowImage, const cv::Mat& flowXY, const cv::Mat &original, int step, const cv::Scalar& color);
+void drawOpticalFlowImage(cv::Mat& flowImage, const cv::Mat &flowX, const cv::Mat &flowY,
+                                 const cv::Mat &original, int step, const cv::Scalar &color);
 static void calculateFlowHS(cv::Mat *result, cv::Mat *resultX, cv::Mat *resultY, const cv::Mat *previous,
+                            const cv::Mat *current, int iterations, double alpha);
+void motionMapHSImage(cv::Mat &flowImage, const cv::Mat &original,  const cv::Mat &previousFrame,
+                           const cv::Mat &currentFrame, double alpha, cv::Mat *velocityX, cv::Mat *velocityY,
+                           cv::Mat *flow);
+void motionMapFImage(cv::Mat &flowImage, const cv::Mat &original, const cv::Mat &previousFrame,
+                            const cv::Mat &currentFrame, cv::Mat *flow);
+void calculateHistogram(const cv::Mat &source, cv::Mat &histogram);
+void balancedHistogramThreshold(const cv::Mat &source, cv::Mat &destination, int criterio = 5);
+int getHistogramWeight(int begin, int end, const cv::Mat &histogram);
+void setDefaultMorphKernel(cv::Mat &kernel, int size, int type = 0);
+
+// Functions
+void drawOpticalFlowImage (cv::Mat& flowImage, const cv::Mat& flowXY, const cv::Mat &original, int step, const cv::Scalar& color)
+{
+    flowImage = original.clone();
+    for(int y = 0; y < flowXY.rows; y += step) {
+        for(int x = 0; x < flowXY.cols; x += step)
+        {
+            const cv::Point2f& fxy = flowXY.at< cv::Point2f>(y, x);
+            cv::line(flowImage, cv::Point(x,y), cv::Point(std::round(x + fxy.x), std::round(y + fxy.y)), color);
+        }
+    }
+}
+
+void drawOpticalFlowImage(cv::Mat& flowImage, const cv::Mat &flowX, const cv::Mat &flowY, const cv::Mat &original,
+                                 int step, const cv::Scalar &color)
+{
+    flowImage = original.clone();
+    for(int y = 0; y < original.rows; y += step) {
+        for(int x = 0; x < original.cols; x += step)
+        {
+            const float& fx = flowX.at<float>(y, x);
+            const float& fy = flowY.at<float>(y, x);
+            cv::line(flowImage, cv::Point(x,y), cv::Point(std::round(x + fx), std::round(y + fy)), color);
+        }
+    }
+}
+
+void calculateFlowHS(cv::Mat *result, cv::Mat *resultX, cv::Mat *resultY, const cv::Mat *previous,
                             const cv::Mat *current, int iterations, double alpha)
 {
     static CvMat *flowX = cvCreateMat(previous->rows, previous->cols, CV_32FC1);
@@ -22,48 +67,106 @@ static void calculateFlowHS(cv::Mat *result, cv::Mat *resultX, cv::Mat *resultY,
     *resultY = cv::Mat(flowY);
 
     // Merge Flows Together
-    static int fromTo[] = {0, 0, 1, 1};
-    cv::Mat combinatition[] = {*resultX, *resultY};
-
-    cv::mixChannels(combinatition, 2, result, 1, fromTo, 2);
+    std::vector<cv::Mat> flows;
+    flows.push_back(*resultX);
+    flows.push_back(*resultY);
+    cv::merge(flows, *result);
 }
 
-static void calculateFlowReMap(cv::Mat *result, cv::Mat *flowXY, const cv::Mat *image)
+void motionMapHSImage(cv::Mat &flowImage, const cv::Mat &original,  const cv::Mat &previousFrame,
+                    const cv::Mat &currentFrame, double alpha, cv::Mat *velocityX, cv::Mat *velocityY, cv::Mat *flow)
 {
-    for (int x = 0; x < flowXY->cols; x++)
-            flowXY->col(x) += cv::Scalar(x, 0);
-
-    for (int y = 0; y < flowXY->rows; y++)
-        flowXY->row(y) += cv::Scalar(0, y);
-
-    cv::remap(*image, *result, *flowXY, cv::Mat(), CV_INTER_CUBIC);
+    calculateFlowHS(flow, velocityX, velocityY, &previousFrame, &currentFrame, 100, alpha);
+    drawOpticalFlowImage(flowImage, *flow, original, 5, CV_RGB(0, 0, 255));
 }
 
-static void calculateFlowImage(cv::Mat &result, const cv::Mat &flowXY)
+void motionMapFImage(cv::Mat &flowImage, const cv::Mat &original, const cv::Mat &previousFrame,
+                     const cv::Mat &currentFrame, cv::Mat *flow)
 {
-    static int fromTo[] = {0,0, 1,1, -1,2};
-    cv::mixChannels(&flowXY, 1, &result, 1, fromTo, 3);
-    result = result*10 + 127;
+    // Calculate flow using Farnebäck's algorithm and some parameters that seem to work the best
+    double pyr_scale = 0.8;
+    double poly_sigma = (previousFrame.rows + previousFrame.cols) / 1000.0;
+    int levels = 100;
+    int winsize = (previousFrame.rows + previousFrame.cols)/100;
+    int iterations = 7;
+    int poly_n = (poly_sigma < 1.5 ? 5 : 7);
+    int flags = 0;
+
+    cv::calcOpticalFlowFarneback(previousFrame, currentFrame, *flow, pyr_scale, levels, winsize, iterations, poly_n,
+                                 poly_sigma, flags);
+    drawOpticalFlowImage(flowImage, *flow, original, 10, CV_RGB(0, 0, 255));
 }
 
-static void getMotionMap(cv::Mat &flowImage, const cv::Mat &previousFrame, const cv::Mat &currentFrame, double alpha,
-                      cv::Mat *velocityX, cv::Mat *velocityY, cv::Mat *flow)
+void calculateHistogram(const cv::Mat &source, cv::Mat &histogram)
 {
-    static cv::Mat flowXY(previousFrame.rows, previousFrame.cols, CV_32FC2);
-    static cv::Mat variance, flowReMaped;
+    int histogramSize = 256;
+    float range[] = {0, 256};
+    const float* histogramRange = { range };
+    int grayChannels = 0;
+    cv::calcHist(&source, 1, &grayChannels, cv::Mat(), histogram, 1, &histogramSize, &histogramRange);
+}
 
-    calculateFlowHS(&flowXY, velocityX, velocityY, &previousFrame, &currentFrame, 100, alpha);
-    calculateFlowReMap(&flowReMaped, &flowXY, &previousFrame);
-    cv::compare(previousFrame, flowReMaped, variance, cv::CMP_EQ);
+int getHistogramWeight(int begin, int end, const cv::Mat &histogram)
+{
+    int range = end -begin;
+    int sum = 0;
+    for(int i = begin; i < end; i++) {
+        sum += static_cast<int>(histogram.at<uchar>(0, i));
+    }
+    return sum / range;
+}
 
-    // combine all the values into a single matrix with velocities and variance
-    *flow = cv::Mat(previousFrame.rows, previousFrame.cols, CV_32FC4);
-    cv::Mat mix[] = {*flow, variance};
-    static int fromTo[] = {0,0, 1,1, 2,2, -1,3};
-    cv::mixChannels(mix, 2, flow, 1, fromTo, 4);
+void balancedHistogramThreshold(const cv::Mat &source, cv::Mat &destination, int criterio)
+{
+    cv::Mat histogram;
+    calculateHistogram(source, histogram);
 
-    flowImage = cv::Mat(previousFrame.rows, previousFrame.cols, CV_32FC4);
-    calculateFlowImage(flowImage, flowXY);
+    int begin = 0, end = 256, center = 128;
+
+    int leftWeight = getHistogramWeight(begin, center + 1, histogram);
+    int rightWeight = getHistogramWeight(center + 1, end, histogram);
+    int newCenter;
+    int diff = 8000;
+
+    int cnt = 0;
+    do {
+        newCenter = (leftWeight + rightWeight) / 2;
+        diff = std::abs(newCenter - center);
+        center = newCenter;
+        leftWeight = getHistogramWeight(begin, center + 1, histogram);
+        rightWeight = getHistogramWeight(center + 1, end, histogram);
+        cnt++;
+    } while(diff >= criterio && cnt < 200);
+
+// METHOD 1
+//    for (;begin <= end;) {
+//        if (rightWeight > leftWeight) {
+//            rightWeight = static_cast<int>(histogram.at<uchar>(0, end--));
+//            if ((begin + end) / 2 < center) {
+//                rightWeight += static_cast<int>(histogram.at<uchar>(0, center));
+//                leftWeight -= static_cast<int>(histogram.at<uchar>(0, center));
+//                center--;
+//            }
+//        } else {
+//            leftWeight = static_cast<int>(histogram.at<uchar>(0, begin++));
+//            if ((begin + end) / 2 >= center) {
+//                leftWeight += static_cast<int>(histogram.at<uchar>(0, center + 1));
+//                rightWeight -= static_cast<int>(histogram.at<uchar>(0, center + 1));
+//                center++;
+//            }
+//        }
+//    }
+
+    cv::threshold(source, destination, center, 255, cv::THRESH_BINARY);
+}
+
+void setDefaultMorphKernel(cv::Mat &kernel, int size, int type)
+{
+    if (type = 0) {
+        kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(size, size));
+    } else {
+        kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(size, size));
+    }
 }
 
 #endif // PROJECT7_H
